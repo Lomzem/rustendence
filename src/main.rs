9633c95856363111f70fc98764932d63a87dae09
Lawjay Lee <lawjaylee1@icloud.com>
2024-10-07 13:39:46 -0700

    more deserialization

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,27 @@
 use serde::Deserialize;
 use std::env;
+use std::error::Error;
+use time::macros::offset;
+use time::util::local_offset;
 use tokio::main;
 
-use reqwest::{Error, Url};
+use reqwest::Url;
 
-const ENDPOINT: &str = "https://canvas.csuchico.edu/api/v1/courses/35697/quizzes";
+const ENDPOINT: &str = "https://canvas.csuchico.edu/api/v1/courses/35697/assignments";
 
 #[derive(Deserialize, Debug)]
-struct Quiz {
-    id: i32,
+struct Assignment {
+    quiz_id: Option<i32>,
+    name: String,
+    #[serde(with = "time::serde::iso8601")]
+    due_at: time::OffsetDateTime,
 }
 
 #[main]
-async fn main() -> Result<(), Error> {
+async fn main() -> Result<(), Box<dyn Error>> {
     let access_token = env::var("CANVAS_ACCESS_TOKEN").unwrap();
     let url = Url::parse_with_params(ENDPOINT, &[("access_token", access_token)]).unwrap();
-    let quizzes: Vec<Quiz> = reqwest::get(url).await?.json().await?;
-    dbg!(&quizzes);
-    // let api_response: Vec<Quiz> = reqwest::Client::new().get(url).send().await?.text();
+    let quizzes: Vec<Assignment> = reqwest::get(url).await?.json().await?;
+    dbg!(&quizzes[0].due_at.to_offset(offset!(-6)));
     Ok(())
 }
