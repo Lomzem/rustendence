efbe580bf1602f114e65d353d02e0108e5aa2091
Lawjay Lee <lawjaylee1@icloud.com>
2024-10-07 13:56:58 -0700

    filters

--- src/main.rs
+++ src/main.rs
@@ -13,15 +13,34 @@ const ENDPOINT: &str = "https://canvas.csuchico.edu/api/v1/courses/35697/assignm
 struct Assignment {
     quiz_id: Option<i32>,
     name: String,
-    #[serde(with = "time::serde::iso8601")]
+    #[serde(with = "time::serde::rfc3339")]
     due_at: time::OffsetDateTime,
+    has_submitted_submissions: bool,
 }
 
 #[main]
 async fn main() -> Result<(), Box<dyn Error>> {
     let access_token = env::var("CANVAS_ACCESS_TOKEN").unwrap();
-    let url = Url::parse_with_params(ENDPOINT, &[("access_token", access_token)]).unwrap();
-    let quizzes: Vec<Assignment> = reqwest::get(url).await?.json().await?;
-    dbg!(&quizzes[0].due_at.to_offset(offset!(-6)));
+    let url = Url::parse_with_params(
+        ENDPOINT,
+        &[
+            ("access_token", access_token.to_string()),
+            ("per_page", "10000".to_string()),
+        ],
+    )
+    .unwrap();
+
+    let quizzes: Vec<Assignment> = reqwest::get(url)
+        .await?
+        .json::<Vec<Assignment>>()
+        .await?
+        .into_iter()
+        .filter(|x| x.quiz_id.is_some())
+        .filter(|x| !x.has_submitted_submissions)
+        .collect();
+
+    // let quizzes = reqwest::get(url).await?.json::<Vec<Assignment>>().await?.iter().map(|f|);
+
+    dbg!(&quizzes);
     Ok(())
 }
